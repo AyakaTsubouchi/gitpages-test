1cf31c85bb01b5795e4ff29fd487a5c9416aa31f
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-02-17 17:03:54 -0800

    Updates

---
+++ precache-manifest.ff89327291830d73c58447bc34625001.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff68cd8efad6955e18319e3bdf66725",
+    "url": "/gitpages-test/index.html"
+  },
+  {
+    "revision": "c94fedc834d488091d23",
+    "url": "/gitpages-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8466f848531adad73624",
+    "url": "/gitpages-test/static/js/2.b4c3fa76.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gitpages-test/static/js/2.b4c3fa76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94fedc834d488091d23",
+    "url": "/gitpages-test/static/js/main.99aa0c6e.chunk.js"
+  },
+  {
+    "revision": "ab50920dabf649ed4a7f",
+    "url": "/gitpages-test/static/js/runtime-main.33030063.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
